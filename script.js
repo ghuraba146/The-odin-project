f0c754d160c0a6285b91f432fcd55cf6d13ed599
ghuraba146 <junaidzahid117@gmail.com>
2025-07-23 20:28:38 +0530

    starting

---
+++ script.js
@@ -0,0 +1,18 @@
+let users = [
+  { name: "Ali", friends: ["Sara", "Zoya",'junaid'] },
+  { name: "Sara", friends: ["Ali", "Rehan",'junaid',"Zoya"] },
+  { name: "Zoya", friends: ["Ali"] },
+  { name: "Rehan", friends: ["Sara"] },
+  { name: "Hassan", friends: ["Zoya", "Rehan"] }
+];
+
+function findMutualFriedns(usersArray){
+  
+ usersArray.forEach(user => {
+  
+ });
+ 
+  
+
+}
+console.log(findMutualFriedns('Ali','Sara',users));
